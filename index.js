bfd370af6e3e6f333633759132f919d54f6203dd
RangerMauve <rangermauve@hotmail.com>
2019-03-14 14:10:56 -0400

    Initial implementation

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function (instance, prefix) {
+  return new SubSignalhub(instance, prefix)
+}
+
+class SubSignalhub {
+  constructor(instance, prefix) {
+    this._instance = instance
+    this._prefix = prefix
+  }
+
+  subscribe(channel) {
+    return this._instance.subscribe(this._prefix + channel)
+  }
+
+  broadcast(channel, message, cb) {
+    return this._instance.broadcast(this._prefix + channel, message, cb)
+  }
+
+  close (cb) {
+    return this._instance.close(cb)
+  }
+}
